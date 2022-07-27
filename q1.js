08b826d5c0a5aeaf058d666e7d8ab8bc4f17fbe7
aptale-designs <81027955+aptale-designs@users.noreply.github.com>
2022-07-27 16:25:51 +0530

    Delete q1.js

--- q1.js
+++
@@ -1,8 +0,0 @@
-// Write a program to print Factorial of any number
-let fact = 1;
-let number = parseInt(prompt("Enter number:"));
-
-for (let i = 1; i <=number; i++) {
-    fact = fact*i;
-}
-alert(fact);
