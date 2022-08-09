87ca80869ee64ebfd0ae4fd7762cd295deb964ed
aptale-designs <81027955+aptale-designs@users.noreply.github.com>
2022-08-09 17:12:48 +0530

    Add files via upload

---
+++ Js Assignment/q9.js
@@ -0,0 +1,17 @@
+const user = {
+    userId: "11001", firstName: "Aparna", LastName: "Chauhan", password: "appu2002", country: "India", continent: "Asia", age: "21", displayAge: function () {
+        return this.age;
+    }, AccountCreated: function () {
+        return "Account Created";
+    }
+};
+console.log(`
+    user id: ${user.userId}
+    First Name: ${user.firstName}
+    Last Name: ${user.LastName}
+    password: ${user.password}
+    Country: ${user.country}
+    Continent: ${user.continent}
+`);
+alert(`Age: ${user.displayAge()}`);
+alert(user.AccountCreated());
