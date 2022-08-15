cbaf219a7e546b219529f14a2c631b0a95da80b3
aptale-designs <81027955+aptale-designs@users.noreply.github.com>
2022-08-15 15:56:29 +0530

    Add files via upload

---
+++ Array-Methods-Assignment/index.js
@@ -0,0 +1,566 @@
+let superMarket = [{
+    "title": "Brown eggs",
+    "type": "dairy",
+    "description": "Raw organic brown eggs in a basket",
+    "filename": "0.jpg",
+    "height": 600,
+    "width": 400,
+    "price": 28.1,
+    "rating": 4
+}, {
+    "title": "Sweet fresh strawberry",
+    "type": "fruit",
+    "description": "Sweet fresh stawberry on the wooden table",
+    "filename": "1.jpg",
+    "height": 450,
+    "width": 299,
+    "price": 29.45,
+    "rating": 4
+}, {
+    "title": "Asparagus",
+    "type": "vegetable",
+    "description": "Asparagus with ham on the wooden table",
+    "filename": "2.jpg",
+    "height": 450,
+    "width": 299,
+    "price": 18.95,
+    "rating": 3
+}, {
+    "title": "Green smoothie",
+    "type": "dairy",
+    "description": "Glass of green smoothie with quail egg's yolk, served with cocktail tube, green apple and baby spinach leaves over tin surface.",
+    "filename": "3.jpg",
+    "height": 600,
+    "width": 399,
+    "price": 17.68,
+    "rating": 4
+}, {
+    "title": "Raw legums",
+    "type": "vegetable",
+    "description": "Raw legums on the wooden table",
+    "filename": "4.jpg",
+    "height": 450,
+    "width": 299,
+    "price": 17.11,
+    "rating": 2
+}, {
+    "title": "Baking cake",
+    "type": "dairy",
+    "description": "Baking cake in rural kitchen - dough recipe ingredients (eggs, flour, sugar) on vintage wooden table from above.",
+    "filename": "5.jpg",
+    "height": 450,
+    "width": 675,
+    "price": 11.14,
+    "rating": 4
+}, {
+    "title": "Pesto with basil",
+    "type": "vegetable",
+    "description": "Italian traditional pesto with basil, chesse and oil",
+    "filename": "6.jpg",
+    "height": 450,
+    "width": 299,
+    "price": 18.19,
+    "rating": 2
+}, {
+    "title": "Hazelnut in black ceramic bowl",
+    "type": "vegetable",
+    "description": "Hazelnut in black ceramic bowl on old wooden background. forest wealth. rustic style. selective focus",
+    "filename": "7.jpg",
+    "height": 450,
+    "width": 301,
+    "price": 27.35,
+    "rating": 0
+}, {
+    "title": "Fresh strawberry",
+    "type": "fruit",
+    "description": "Sweet fresh stawberry on the wooden table",
+    "filename": "8.jpg",
+    "height": 600,
+    "width": 399,
+    "price": 28.59,
+    "rating": 4
+}, {
+    "title": "Lemon and salt",
+    "type": "fruit",
+    "description": "Rosemary, lemon and salt on the table",
+    "filename": "9.jpg",
+    "height": 450,
+    "width": 299,
+    "price": 15.79,
+    "rating": 5
+}, {
+    "title": "Homemade bread",
+    "type": "bakery",
+    "description": "Homemade bread",
+    "filename": "10.jpg",
+    "height": 450,
+    "width": 301,
+    "price": 17.48,
+    "rating": 3
+}, {
+    "title": "Legums",
+    "type": "vegetable",
+    "description": "Cooked legums on the wooden table",
+    "filename": "11.jpg",
+    "height": 600,
+    "width": 399,
+    "price": 14.77,
+    "rating": 0
+}, {
+    "title": "Fresh tomato",
+    "type": "vegetable",
+    "description": "Fresh tomato juice with basil",
+    "filename": "12.jpg",
+    "height": 600,
+    "width": 903,
+    "price": 16.3,
+    "rating": 2
+}, {
+    "title": "Healthy breakfast",
+    "type": "fruit",
+    "description": "Healthy breakfast set. rice cereal or porridge with berries and honey over rustic wood background",
+    "filename": "13.jpg",
+    "height": 450,
+    "width": 350,
+    "price": 13.02,
+    "rating": 2
+}, {
+    "title": "Green beans",
+    "type": "vegetable",
+    "description": "Raw organic green beans ready to eat",
+    "filename": "14.jpg",
+    "height": 450,
+    "width": 300,
+    "price": 28.79,
+    "rating": 1
+}, {
+    "title": "Baked stuffed portabello mushrooms",
+    "type": "bakery",
+    "description": "Homemade baked stuffed portabello mushrooms with spinach and cheese"
+    ,
+    "filename": "15.jpg",
+    "height": 600,
+    "width": 400,
+    "price": 20.31,
+    "rating": 1
+}, {
+    "title": "Strawberry jelly",
+    "type": "fruit",
+    "description": "Homemade organic strawberry jelly in a jar",
+    "filename": "16.jpg",
+    "height": 400,
+    "width": 600,
+    "price": 14.18,
+    "rating": 1
+}, {
+    "title": "Pears juice",
+    "type": "fruit",
+    "description": "Fresh pears juice on the wooden table",
+    "filename": "17.jpg",
+    "height": 600,
+    "width": 398,
+    "price": 19.49,
+    "rating": 4
+}, {
+    "title": "Fresh pears",
+    "type": "fruit",
+    "description": "Sweet fresh pears on the wooden table",
+    "filename": "18.jpg",
+    "height": 600,
+    "width": 398,
+    "price": 15.12,
+    "rating": 5
+}, {
+    "title": "Caprese salad",
+    "type": "vegetable",
+    "description": "Homemade healthy caprese salad with tomato mozzarella and basil",
+    "filename": "19.jpg",
+    "height": 400,
+    "width": 600,
+    "price": 16.76,
+    "rating": 5
+}, {
+    "title": "Oranges",
+    "type": "fruit",
+    "description": "Orange popsicle ice cream bars made from fresh oranges. a refreshing summer treat.",
+    "filename": "20.jpg",
+    "height": 450,
+    "width": 274,
+    "price": 21.48,
+    "rating": 4
+}, {
+    "title": "Vegan food",
+    "type": "vegetable",
+    "description": "Concept of vegan food",
+    "filename": "21.jpg",
+    "height": 450,
+    "width": 299,
+    "price": 29.66,
+    "rating": 4
+}, {
+    "title": "Breakfast with muesli",
+    "type": "dairy",
+    "description": "Concept of healthy breakfast with muesli",
+    "filename": "22.jpg",
+    "height": 450,
+    "width": 299,
+    "price": 22.7,
+    "rating": 2
+}, {
+    "title": "Honey",
+    "type": "bakery",
+    "description": "Honey and honey cell on the table",
+    "filename": "23.jpg",
+    "height": 450,
+    "width": 299,
+    "price": 17.01,
+    "rating": 2
+}, {
+    "title": "Breakfast with cottage",
+    "type": "fruit",
+    "description": "Healthy breakfast with cottage cheese and strawberry",
+    "filename": "24.jpg",
+    "height": 600,
+    "width": 398,
+    "price": 14.05,
+    "rating": 1
+}, {
+    "title": "Strawberry smoothie",
+    "type": "fruit",
+    "description": "Glass of red strawberry smoothie with chia seeds, served with retro cocktail tube, fresh mint and strawberries over dark background",
+    "filename": "25.jpg",
+    "height": 600,
+    "width": 400,
+    "price": 28.86,
+    "rating": 2
+}, {
+    "title": "Strawberry and mint",
+    "type": "fruit",
+    "description": "Homemade muesli with strawberry and mint",
+    "filename": "26.jpg",
+    "height": 450,
+    "width": 299,
+    "price": 26.21,
+    "rating": 4
+}, {
+    "title": "Ricotta",
+    "type": "dairy",
+    "description": "Ricotta with berry and mint",
+    "filename": "27.jpg",
+    "height": 600,
+    "width": 398,
+    "price": 27.81,
+    "rating": 5
+}, {
+    "title": "Cuban sandwich",
+    "type": "bakery",
+    "description": "Homemade traditional cuban sandwiches with ham pork and cheese",
+    "filename": "28.jpg",
+    "height": 450,
+    "width": 300,
+    "price": 18.5,
+    "rating": 4
+}, {
+    "title": "Granola",
+    "type": "dairy",
+    "description": "Glass jar with homemade granola and yogurt with nuts, raspberries and blackberries on wooden cutting board over white textile in day light",
+    "filename": "29.jpg",
+    "height": 450,
+    "width": 300,
+    "price": 29.97,
+    "rating": 3
+}, {
+    "title": "Smoothie with chia seeds",
+    "type": "fruit",
+    "description": "Glass of red strawberry smoothie with chia seeds, served with retro cocktail tube, fresh mint and strawberries over wooden table",
+    "filename": "30.jpg",
+    "height": 600,
+    "width": 900,
+    "price": 25.26,
+    "rating": 5
+}, {
+    "title": "Yogurt",
+    "type": "dairy",
+    "description": "Homemade yogurt with raspberry and mint",
+    "filename": "31.jpg",
+    "height": 450,
+    "width": 299,
+    "price": 27.61,
+    "rating": 4
+}, {
+    "title": "Sandwich with salad",
+    "type": "vegetable",
+    "description": "Vegan sandwich with salad, tomato and radish",
+    "filename": "32.jpg",
+    "height": 600,
+    "width": 398,
+    "price": 22.48,
+    "rating": 5
+}, {
+    "title": "Cherry",
+    "type": "fruit",
+    "description": "Cherry with sugar on old table",
+    "filename": "33.jpg",
+    "height": 600,
+    "width": 400,
+    "price": 14.35,
+    "rating": 5
+}, {
+    "title": "Raw asparagus",
+    "type": "vegetable",
+    "description": "Raw fresh asparagus salad with cheese and dressing",
+    "filename": "34.jpg",
+    "height": 600,
+    "width": 400,
+    "price": 22.97,
+    "rating": 4
+}, {
+    "title": "Corn",
+    "type": "vegetable",
+    "description": "Grilled corn on the cob with salt and butter",
+    "filename": "35.jpg",
+    "height": 450,
+    "width": 300,
+    "price": 13.55,
+    "rating": 2
+}, {
+    "title": "Vegan",
+    "type": "vegan",
+    "description": "Concept of healthy vegan eating",
+    "filename": "36.jpg",
+    "height": 600,
+    "width": 398,
+    "price": 28.96,
+    "rating": 5
+}, {
+    "title": "Fresh blueberries",
+    "type": "fruit",
+    "description": "Healthy breakfast. berry crumble with fresh blueberries, raspberries, strawberries, almond, walnuts, pecans, yogurt, and mint in ceramic plates over white wooden surface, top view",
+    "filename": "37.jpg",
+    "height": 450,
+    "width": 321,
+    "price": 21.01,
+    "rating": 4
+}, {
+    "title": "Smashed avocado",
+    "type": "fruit",
+    "description": "Vegan sandwiches with smashed avocado, tomatoes and radish. top view",
+    "filename": "38.jpg",
+    "height": 450,
+    "width": 450,
+    "price": 25.88,
+    "rating": 0
+}, {
+    "title": "Italian ciabatta",
+    "type": "bakery",
+    "description": "Italian ciabatta bread cut in slices on wooden chopping board with herbs, garlic and olives over dark grunge backdrop, top view",
+    "filename": "39.jpg",
+    "height": 450,
+    "width": 565,
+    "price": 15.18,
+    "rating": 1
+}, {
+    "title": "Rustic breakfast",
+    "type": "dairy",
+    "description": "Rustic healthy breakfast set. cooked buckwheat groats with milk and honey on dark grunge backdrop. top view, copy space",
+    "filename": "40.jpg",
+    "height": 450,
+    "width": 307,
+    "price": 21.32,
+    "rating": 0
+}, {
+    "title": "Sliced lemons",
+    "type": "fruit",
+    "description": "Heap of whole and sliced lemons and limes with mint in vintage metal grid box over old wooden table with turquoise wooden background. dark rustic style.",
+    "filename": "41.jpg",
+    "height": 600,
+    "width": 900,
+    "price": 27.14,
+    "rating": 2
+}, {
+    "title": "Plums",
+    "type": "fruit",
+    "description": "Yellow and red sweet plums",
+    "filename": "42.jpg",
+    "height": 450,
+    "width": 299,
+    "price": 19.18,
+    "rating": 1
+}, {
+    "title": "French fries",
+    "type": "bakery",
+    "description": "Homemade oven baked french fries with ketchup",
+    "filename": "43.jpg",
+    "height": 600,
+    "width": 400,
+    "price": 18.32,
+    "rating": 3
+}, {
+    "title": "Strawberries",
+    "type": "fruit",
+    "description": "Healthy breakfast set. rice cereal or porridge with fresh strawberry, apricots, almond and honey over white rustic wood backdrop, top view, \u0000",
+    "filename": "44.jpg",
+    "height": 450,
+    "width": 352,
+    "price": 15.13,
+    "rating": 3
+}, {
+    "title": "Ground beef meat burger",
+    "type": "meat",
+    "description": "Raw ground beef meat burger steak cutlets with seasoning on vintage wooden boards, black background",
+    "filename": "45.jpg",
+    "height": 450,
+    "width": 675,
+    "price": 11.73,
+    "rating": 5
+}, {
+    "title": "Tomatoes",
+    "type": "fruit",
+    "description": "Organic tomatoes made with love",
+    "filename": "46.jpg",
+    "height": 450,
+    "width": 675,
+    "price": 26.03,
+    "rating": 4
+}, {
+    "title": "Basil",
+    "type": "vegetable",
+    "description": "Concept of vegan food with basil",
+    "filename": "47.jpg",
+    "height": 450,
+    "width": 678,
+    "price": 15.19,
+    "rating": 4
+}, {
+    "title": "Fruits bouquet",
+    "type": "fruit",
+    "description": "Abstract citrus fruits bouquet on blue background",
+    "filename": "48.jpg",
+    "height": 600,
+    "width": 401,
+    "price": 18.18,
+    "rating": 1
+}, {
+    "title": "Peaches on branch",
+    "type": "fruit",
+    "description": "Peaches on branch with leaves and glasses with peach juice and limonade with ice cubes in aluminum tray over old metal table. dark rustic style. top view.",
+    "filename": "49.jpg",
+    "height": 600,
+    "width": 400,
+    "price": 25.62,
+    "rating": 3
+}];
+
+document.write("<h1>find()</h1>");
+//1. Find the element with rating >= 3
+document.write("<h2>1. Find the element with rating >= 3</h2>");
+let obj = superMarket.find(o => o.rating >= 3);
+document.write(JSON.stringify(obj));
+
+//2. Find the element with type ‘vegetable’
+document.write("<h2>2. Find the element with type ‘vegetable’</h2>");
+let obj2 = superMarket.find(o => o.type === 'vegetable');
+document.write(JSON.stringify(obj2));
+
+// 3. Find the element with price < 15
+document.write("<h2>3. Find the element with price < 15</h2>");
+let obj3 = superMarket.find(o => o.price < 15);
+document.write(JSON.stringify(obj3));
+
+// 4. Find the element with title which contains the word ‘Lemon’
+document.write("<h2>4. Find the element with title which contains the word ‘Lemon’</h2>");
+let obj4 = superMarket.find(o => o.title.includes("Lemon", 0));
+document.write(JSON.stringify(obj4));
+
+// 5. Find the element with height = 450 and width = 450
+document.write("<h2>5. Find the element with height = 450 and width = 450</h2>");
+let obj5 = superMarket.find(o => o.height === 450 && o.width === 450);
+document.write(JSON.stringify(obj5));
+
+
+document.write("<h1>filter()</h1>");
+// 1. Filter the elements with rating < 2
+document.write("<h2>6. Filter the elements with rating < 2 </h2>");
+let obj6 = superMarket.filter(o => o.rating < 2);
+document.write(JSON.stringify(obj6));
+
+//2. Filter the elements with type ‘dairy’
+document.write("<h2>2. Filter the elements with type ‘dairy’</h2>");
+let obj7 = superMarket.filter(o => o.type === "dairy");
+document.write(JSON.stringify(obj7));
+
+// 3. Filter the elements with price >20</h2>
+document.write("<h2>3. Filter the elements with price >20</h2>");
+let obj8 = superMarket.filter(o => o.price > 20);
+document.write(JSON.stringify(obj8));
+
+// 4. Filter the elements with title which contains the word ‘with’
+document.write("<h2>4. Filter the elements with title which contains the word ‘with’</h2>");
+let obj9 = superMarket.filter(o => o.title.includes("with",0));
+document.write(JSON.stringify(obj9));
+
+
+// 5. Filter the elements with description which contains the word ‘with’
+document.write("<h2>5. Filter the elements with description which contains the word ‘with’</h2>");
+let obj10 = superMarket.filter(o => o.description.includes("with",0));
+document.write(JSON.stringify(obj10));
+
+// 6. Filter the elements with height = 600 and width = 400
+document.write("<h2>6. Filter the elements with height = 600 and width = 400</h2>");
+let obj11 = superMarket.filter(o => o.height === 600 && o.width === 400);
+document.write(JSON.stringify(obj11));
+
+document.write("<h1>map()</h1>");
+//1. Add the property ‘isAvailable: true’ to all the elements
+document.write("<h2>1. Add the property ‘isAvailable: true’ to all the elements</h2>");
+let obj12 = superMarket.map(obj => ({ ...obj, isAvailable: 'true' }))
+document.write(JSON.stringify(obj12));
+
+document.write("<h1>reduce()</h1>");
+// 1. Calculate the averageRatings of all the element
+document.write("<h2>1. Calculate the averageRatings of all the element</h2>");
+const averageRating = superMarket.reduce((a, b) => a + b.rating, 0) / superMarket.length;
+document.write("Average Rating is: " + averageRating);
+
+// 2. Calculate the averageHeight of all the elemen
+document.write("<h2>2. Calculate the averageHeight of all the element</h2>");
+const averageHeight = superMarket.reduce((a, b) => a + b.height, 0) / superMarket.length;
+document.write("Average Height is: " + averageHeight);
+
+// 3. Calculate the averageWidth of all the element
+document.write("<h2>3. Calculate the averageWidth of all the element</h2>");
+const averageWidth = superMarket.reduce((a, b) => a + b.width, 0) / superMarket.length;
+document.write("Average Width is: " + averageWidth);
+
+// 4. Calculate the averagePrice of all the elemen
+document.write("<h2>4. Calculate the averagePrice of all the element</h2>");
+const averagePrice = superMarket.reduce((a, b) => a + b.price, 0) / superMarket.length;
+document.write("Average Price is: " + averagePrice);
+
+document.write("<h1>Mix of array methods</h1>");
+// 1. Filter the elements of type: ‘dairy’ and calculate averageRatings
+document.write("<h2>1. Filter the elements of type: ‘dairy’ and calculate averageRatings</h2>");
+let mix1 = superMarket.filter(o => o.type === "dairy");
+let mix2 = superMarket.reduce((a, b) => a + b.rating, 0) / superMarket.length;
+document.write(JSON.stringify(mix1));
+document.write("<br><b>Average Rating is: " + mix2 + "</b>");
+
+// 2. Filter the elements of type: ‘vegetable’ and calculate averagePrice
+document.write("<h2>2. Filter the elements of type: ‘vegetable’ and calculate averagePrice</h2>");
+let mix3 = superMarket.filter(o => o.type === "vegetable");
+let mix4 = superMarket.reduce((a, b) => a + b.price, 0) / superMarket.length;
+document.write(JSON.stringify(mix3));
+document.write("<br><b>Average Price is: " + mix4 + "</b>");
+
+//3. Filter the elements of type: ‘fruit’ and add new property ‘isAvailable: true’
+document.write("<h2>3. Filter the elements of type: ‘fruit’ and add new property ‘isAvailable: true’</h2>");
+let mix5 = superMarket.filter(o => o.type === "fruit").map(obj => ({ ...obj, isAvailable: 'true' }));
+document.write(JSON.stringify(mix5));
+
+
+//4. Filter the elements of type: ‘dairy’ and ‘vegetable’ and add new property ‘isAvailable: true’to the filtered elements under type: ‘dairy’ and add new property ‘isAvailable: false’ to the filtered elements under type: ‘vegetable’
+document.write("<h2>4. Filter the elements of type: ‘dairy’ and ‘vegetable’ and add new property ‘isAvailable: true’to the filtered elements under type: ‘dairy’ and add new property ‘isAvailable: false’ to thefiltered elements under type: ‘vegetable’</h2>");
+let mix6 = superMarket.filter(o => o.type === "dairy").map(obj => ({ ...obj, isAvailable: 'true' }));
+let mix7 = superMarket.filter(o => o.type === "vegetable").map(obj => ({ ...obj, isAvailable: 'false' }));
+document.write(JSON.stringify(mix6));
+document.write(JSON.stringify(mix7));
